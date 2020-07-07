2951cbe3bac9fa1f5f6ef4f599779574ea749817
highdef1 <ryancallebaut@gmail.com>
2020-07-07 18:17:05 +0000

    Completed project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 pygame.init()
 
@@ -14,12 +14,34 @@ fish_image = pygame.transform.smoothscale(fish_image, (80, 80))
 fish_rect = fish_image.get_rect()
 fish_rect.center = (width // 2, height // 2)
 
+speed = pygame.math.Vector2(0, 10)
+rotation = random.randint(0, 360)
+speed.rotate_ip(rotation)
+fish_image = pygame.transform.rotate(fish_image, 180 - rotation)
+
+def fish_move():
+  global fish_image 
+  fish_rect.move_ip(speed)
+  screen_info = pygame.display.Info()
+  if fish_rect.right > screen_info.current_w or fish_rect.left < 0:
+    speed[0] *= -1
+    fish_image = pygame.transform.flip(fish_image, True, False)
+    fish_rect.move_ip(speed[0], 0)
+  if fish_rect.top < 0 or fish_rect.bottom > screen_info.current_h:
+    speed[1] *= -1
+    fish_image = pygame.transform.flip(fish_image, False, True)
+    fish_rect.move_ip(0, speed[1])
+
+
 def main():
   while True:
     clock.tick(60)
     screen.fill(color)
     screen.blit(fish_image, fish_rect)
+    fish_move()
     pygame.display.flip()
+
+
 if __name__ == "__main__":
   main()
 
